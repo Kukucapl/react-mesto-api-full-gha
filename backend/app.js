c57ad5b85fb91e034ba995f887c888de9f13fa3e
Ekaterina Kostyukova <kukucapl@yandex.ru>
2023-07-01 05:00:27 +0300

    Почти не срамота

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,13 @@ mongoose.connect(DB_URL)
 
 app.use(bodyParser.json());
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', loginValidator, login);
 app.post('/signup', createUserValidator, createUser);
 
